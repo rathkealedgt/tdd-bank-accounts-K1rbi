059dd8c2053f3e33108265bc71f98fe8031bf34a
K1rbi <51098884+K1rbi@users.noreply.github.com>
2020-05-17 12:00:58 +1200

    Tdd bank start
    - overloads ?

--- BankAccountTest/UnitTest1.vb
+++ BankAccountTest/UnitTest1.vb
@@ -4,6 +4,53 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 <TestClass()> Public Class UnitTest1
 
     <TestMethod()> Public Sub TestMethod1()
+
+        ' round 2 because i forgot to save :(
+
+
+        'arrange
+        Dim AcountHolder As String = "Ms  I. N. Cognito"
+        Dim AcountNumber As String = "ABCD 89011 11167890"
+        Dim Country As String = "Isle of Man"
+
+        Dim Balance As Double = 1043.82
+        Dim IntrestRate As Double = 4.3
+
+
+        ' act
+        Dim Account1 As New BankAccounts.BankAccount(AcountHolder, AcountNumber, Balance, IntrestRate, Country)
+        Dim Account2 As New BankAccounts.BankAccount(AcountHolder, AcountNumber, Balance, IntrestRate)
+        Dim Account3 As New BankAccounts.BankAccount(AcountHolder, AcountNumber, Balance)
+
+
+        ' Assert
+        Assert.IsNotNull(Account1)
+        Assert.IsNotNull(Account2)
+        Assert.IsNotNull(Account3)
+
+    End Sub
+
+
+    <TestMethod()> Public Sub TestAccountHolder()
+        '
+
+        'arrange
+        Dim AcountHolder As String = "Ms  I. N. Cognito"
+        Dim AcountNumber As String = "ABCD 89011 11167890"
+        Dim Country As String = "Isle of Man"
+
+        Dim Balance As Double = 1043.82
+        Dim IntrestRate As Double = 4.3
+
+        Dim Account1 As New BankAccounts.BankAccount(AcountHolder, AcountNumber, Balance, IntrestRate, Country)
+
+        'act
+        Dim Name As String = Account1.GetAccountHolder()
+
+        'assert
+        Assert.AreEqual(Name, "Ms  I. N. Cognito")
+
+
     End Sub
 
 End Class
---
+++ BankAccounts/BankAccount.vb
@@ -0,0 +1,41 @@
+﻿Public Class BankAccount
+    ''' <summary>
+    '''        Bank account
+    ''' </summary>
+    ''' <author> Tom K </author>
+    ''' <date>17/05/2020</date>
+
+    Private AcountHolder As String
+    Private AcountNumber As String
+    Private Country As String
+    Private Balance As Double
+    Private Intrestrate As Double
+
+    Public Sub New(AcountHolder As String, AcountNumber As String, Balance As Double, IntrestRate As Double, Country As String)
+        Me.AcountHolder = AcountHolder
+        Me.AcountHolder = AcountNumber
+        Me.Balance = Balance
+        Me.Intrestrate = IntrestRate
+        Me.Country = Country
+    End Sub
+
+    Public Sub New(AcountHolder As String, AcountNumber As String, Balance As Double, IntrestRate As Double)
+        Me.AcountHolder = AcountHolder
+        Me.AcountHolder = AcountNumber
+        Me.Balance = Balance
+        Me.Intrestrate = IntrestRate
+    End Sub
+
+    Public Sub New(AcountHolder As String, AcountNumber As String, Balance As Double,)
+        Me.AcountHolder = AcountHolder
+        Me.AcountHolder = AcountNumber
+        Me.Balance = Balance
+    End Sub
+
+    Public Function GetAccountHolder()
+
+        Return AcountHolder
+    End Function
+
+
+End Class
