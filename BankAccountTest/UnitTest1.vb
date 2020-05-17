8d8654a712182bd4c54ec6d9ba10dcf06ceffb3a
K1rbi <51098884+K1rbi@users.noreply.github.com>
2020-05-17 21:03:15 +1200

    ok

--- BankAccountTest/UnitTest1.vb
+++ BankAccountTest/UnitTest1.vb
@@ -48,9 +48,166 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
         Dim Name As String = Account1.GetAccountHolder()
 
         'assert
-        Assert.AreEqual(Name, "Ms  I. N. Cognito")
+        Assert.AreEqual(Name, "MS  I. N. Cognito")
 
 
     End Sub
 
+    <TestMethod()> Public Sub TestsetAccountholder()
+        Dim Account1 As BankAccounts.BankAccount = Me.NewAccount
+        '
+
+        'act
+        Account1.SetAccountHolder("Ms  I. N. Cognito")
+
+        'assert
+        Assert.AreEqual(Account1.GetAccountHolder, "Ms  I. N. Cognito")
+
+    End Sub
+
+
+
+    Private Function NewAccount()
+        Dim AcountHolder As String = "Ms  I. N. Cognito"
+        Dim AcountNumber As String = "ABCD 89011 11167890"
+        Dim Country As String = "Isle of Man"
+
+        Dim Balance As Double = 1043.82
+        Dim IntrestRate As Double = 4.3
+
+        Return New BankAccounts.BankAccount(AcountHolder, AcountNumber, Balance, IntrestRate, Country)
+    End Function
+
+    'getters new
+    <TestMethod()> Public Sub TestgetInterestRate()
+        Dim Account1 As BankAccounts.BankAccount = Me.NewAccount
+        '
+
+        'act
+        Dim Interestrate As Double = 4.3
+
+        'assert
+        Assert.AreEqual(Interestrate, Account1.GetInterestRate)
+
+    End Sub
+
+
+
+    <TestMethod()> Public Sub TestSetInterestRate()
+        'arrange
+        Dim Account1 As BankAccounts.BankAccount = Me.NewAccount
+
+        'act
+        Account1.SetInterestRate(5.1)
+
+
+
+        'assert
+        Assert.AreEqual(5.1, Account1.GetInterestRate)
+
+    End Sub
+
+    <TestMethod()> Public Sub TestgetAccountNumber()
+        Dim Account1 As BankAccounts.BankAccount = Me.NewAccount
+        '
+
+        'act
+        Dim AcccountNumber As String = "ABCD 89011 11167890"
+
+        'assert
+        Assert.AreEqual(AcccountNumber, Account1.GetAccountNumber)
+
+    End Sub
+
+
+
+    <TestMethod()> Public Sub TestSetAccountNumber()
+        'arrange
+        Dim Account1 As BankAccounts.BankAccount = Me.NewAccount
+
+        'act
+        Account1.SetAccountNumber("ABCD 89011 11167890")
+
+
+
+        'assert
+        Assert.AreEqual("ABCD 89011 11167890", Account1.GetAccountNumber)
+
+    End Sub
+
+    ' country
+
+    <TestMethod()> Public Sub Testgetcountry()
+        Dim Account1 As BankAccounts.BankAccount = Me.NewAccount
+        '
+
+        'act
+        Dim country As String = "Isle of Man"
+
+        'assert
+        Assert.AreEqual(country, Account1.GetCountry)
+
+    End Sub
+
+
+
+    <TestMethod()> Public Sub TestSetcountry()
+        'arrange
+        Dim Account1 As BankAccounts.BankAccount = Me.NewAccount
+
+        'act
+        Account1.SetCountry("Isle of Man")
+
+
+
+        'assert
+        Assert.AreEqual("Isle of Man", Account1.GetCountry)
+
+    End Sub
+
+
+    ' balance
+    <TestMethod()> Public Sub Testgetbalance()
+        Dim Account1 As BankAccounts.BankAccount = Me.NewAccount
+        '
+
+        'act
+        Dim balance As Double = 1043.82
+
+        'assert
+        Assert.AreEqual(balance, Account1.Getbalance)
+
+    End Sub
+
+
+
+    <TestMethod()> Public Sub TestSetbalance()
+        'arrange
+        Dim Account1 As BankAccounts.BankAccount = Me.NewAccount
+
+        'act
+        Account1.Setbalance(1500)
+        Dim balance As Double = 1500
+
+
+        'assert
+        Assert.AreEqual(balance, Account1.Getbalance)
+
+    End Sub
+
+    <TestMethod()> Public Sub TestApplyInterestRate()
+        'p x r x t = i => 37.07 + 10343.82 = 10380.89
+
+        'arrange
+        Dim account1 As BankAccounts.BankAccount = Me.NewAccount()
+
+        'act
+        account1.Setbalance(10343.82)
+        account1.ApplyInterest()
+
+        'assert
+        Assert.AreEqual(account1.Getbalance(), 10380.89)
+
+
+    End Sub
 End Class
--- BankAccounts/BankAccount.vb
+++ BankAccounts/BankAccount.vb
@@ -13,7 +13,7 @@
 
     Public Sub New(AcountHolder As String, AcountNumber As String, Balance As Double, IntrestRate As Double, Country As String)
         Me.AcountHolder = AcountHolder
-        Me.AcountHolder = AcountNumber
+        Me.AcountNumber = AcountNumber
         Me.Balance = Balance
         Me.Intrestrate = IntrestRate
         Me.Country = Country
@@ -21,20 +21,87 @@
 
     Public Sub New(AcountHolder As String, AcountNumber As String, Balance As Double, IntrestRate As Double)
         Me.AcountHolder = AcountHolder
-        Me.AcountHolder = AcountNumber
+        Me.AcountNumber = AcountNumber
         Me.Balance = Balance
         Me.Intrestrate = IntrestRate
     End Sub
 
-    Public Sub New(AcountHolder As String, AcountNumber As String, Balance As Double,)
+    Public Sub New(AcountHolder As String, AcountNumber As String, Balance As Double)
         Me.AcountHolder = AcountHolder
-        Me.AcountHolder = AcountNumber
+        Me.AcountNumber = AcountNumber
         Me.Balance = Balance
     End Sub
 
     Public Function GetAccountHolder()
 
-        Return AcountHolder
+        Return Me.AcountHolder
+    End Function
+    Public Function SetAccountHolder(AccountHolder As String)
+        Me.AcountHolder = AccountHolder
+
+        Return Nothing
+    End Function
+
+
+
+    Public Function GetInterestRate()
+
+        Return Me.Intrestrate
+    End Function
+
+    Public Function SetInterestRate(rate As Double)
+        Me.Intrestrate = rate
+
+        Return Nothing
+    End Function
+
+
+
+    Public Function GetAccountNumber()
+
+        Return Me.AcountNumber
+    End Function
+
+    Public Function SetAccountNumber(Number As String)
+        Me.AcountNumber = Number
+
+        Return Nothing
+    End Function
+
+    'Country
+    Public Function GetCountry()
+
+        Return Me.Country
+    End Function
+
+    Public Function SetCountry(country As String)
+        Me.Country = country
+
+        Return Nothing
+    End Function
+
+    'balance
+
+
+    Public Function Getbalance()
+
+        Return Me.Balance
+    End Function
+
+    Public Function Setbalance(balance As String)
+        Me.Balance = balance
+
+        Return Nothing
+    End Function
+
+    Public Function ApplyInterest()
+        Dim NewBalance As Double
+        NewBalance = Me.Balance + (Me.Balance * 0.043 * (1 / 12))
+
+        ' 2dp
+        Me.Balance = Math.Round(NewBalance, 2)
+
+        Return Nothing
     End Function
 
 
