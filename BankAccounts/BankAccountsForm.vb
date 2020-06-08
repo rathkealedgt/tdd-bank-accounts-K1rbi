6580900bb16f1a73818df9acec9f15a80f0ef7e8
K1rbi <51098884+K1rbi@users.noreply.github.com>
2020-06-08 21:58:08 +1200

    Buttons

--- BankAccounts/BankAccountsForm.vb
+++ BankAccounts/BankAccountsForm.vb
@@ -1,4 +1,6 @@
-﻿Public Class BankAccountsForm
+﻿Imports System.Text
+
+Public Class BankAccountsForm
 
     Private Accounts(4) As BankAccount
     Private NumAccounts As Integer
@@ -40,9 +42,6 @@
         Return Nothing
     End Function
 
-    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
-        End
-    End Sub
 
     Public Function SetTestForTesting(AccountHolder As String, AccountNumber As String, Balance As String, InterestRate As String, Country As String)
 
@@ -60,4 +59,66 @@
         Return Me.Accounts
     End Function
 
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        End
+    End Sub
+
+
+    Private Sub BtnCreateAccount_Click(sender As Object, e As EventArgs) Handles BtnCreateAccount.Click
+
+        Dim FeeBack As String = "Account added."
+        Dim Title As String = "Success"
+
+        Try
+            Me.CreatAccount()
+
+        Catch EX As Exception
+            Title = EX.Message()
+
+            If EX.Message = "AccountHolderRequiredExpection" Then
+                FeeBack = "please enter an account holder required"
+                TxtAccountHolder.Focus()
+
+            ElseIf EX.Message = "AccountNumberRequired" Then
+                FeeBack = "please enter an account number required"
+                TxtAccountNumber.Focus()
+            ElseIf EX.Message = "AccoubtCountryRequired" Then
+                FeeBack = "please enter an account Country required"
+                TxtCountry.Focus()
+
+            ElseIf EX.Message = "AccountInterestRateRequired" Then
+                FeeBack = "please enter an account interest Rate required"
+                TxtAccountInterestRate.Focus()
+
+            ElseIf EX.Message = "AccountBalanceRequired" Then
+                FeeBack = "please enter an account balance required"
+                TxtAccountBalance.Focus()
+
+            ElseIf EX.Message = "MaxumumACcountsReachedException" Then
+                FeeBack = "you have reached the max amount of accounts"
+                TxtCountry.Focus()
+            Else
+                Title = "An error occurred"
+                FeeBack = EX.Message
+            End If
+        End Try
+
+        MsgBox(FeeBack, vbOKOnly, Title)
+
+    End Sub
+
+    Private Sub BtnPrint_Click(sender As Object, e As EventArgs) Handles BtnPrint.Click
+        Dim AllAccounts As New StringBuilder()
+
+        For Each Ba As BankAccount In Me.Accounts
+            If Ba Is Nothing Then Exit For
+
+            AllAccounts.Append(Ba.ToString())
+            AllAccounts.Append(vbCrLf)
+
+        Next
+        TxtAccountList.Text = AllAccounts.ToString()
+
+
+    End Sub
 End Class
